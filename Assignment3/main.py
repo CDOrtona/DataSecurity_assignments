94cfc4b05bebdb18c118f941643319c8b196cf9b
CDOrtona <github@cdmails.anonaddy.com>
2023-05-11 15:53:22 +0200

    major updates

--- Assignment3/main.py
+++ Assignment3/main.py
@@ -2,6 +2,8 @@ from Cryptodome.Cipher import AES
 from Cryptodome.Util.Padding import pad, unpad
 from matplotlib import pyplot as pl
 import numpy as np
+from rsa import RSA
+from utils import read_bin
 
 
 def block_cipher():
@@ -9,6 +11,7 @@ def block_cipher():
     # cipher = AES.new(key, AES.MODE_ECB)
     sender = AES.new(key, AES.MODE_CFB)
     receiver = AES.new(key, AES.MODE_CFB, sender.IV)
+    print(f'AES block size -> {AES.block_size}')
     # cipher = AES.new(key, AES.MODE_CFB)
     # cipher = AES.new(key, AES.MODE_CTR)
     plaintextA = b'this is a random message'
@@ -17,8 +20,7 @@ def block_cipher():
     plain_text = unpad(receiver.decrypt(cipher_text), AES.block_size)
     print(plain_text)
 
-    with open(file='secret_message.bin', mode='rb') as f:
-        vectorized_im = f.read()
+    vectorized_im = read_bin('secret_message.bin')
 
     cipher_im = sender.encrypt(vectorized_im)
     plain_im_unrolled = np.reshape(np.array(list(receiver.decrypt(cipher_im))), (2048, 2912))
@@ -26,6 +28,23 @@ def block_cipher():
     pl.show()
 
 
-if __name__ == "__main__":
-    block_cipher()
+def message_exc():
+    k_AES = b'0123456701234567'
 
+    Bob = RSA(key_length=128, mrt_trials=1000)
+    Alice = RSA(k_pub=Bob.key_pub)
+
+    k_aes_enc = Alice.encrypt(int.from_bytes(k_AES, "big"))
+
+    y = read_bin('ciphertext_AES.bin')
+
+    Bob_AES = AES.new(Bob.decrypt(k_aes_enc).to_bytes(Bob.key_length // 8, "big"), AES.MODE_ECB)
+    x = Bob_AES.decrypt(y)
+
+    print(Alice, Bob)
+    print(f'---- Plaintext ---- \n {x}')
+
+
+if __name__ == "__main__":
+    # block_cipher()
+    message_exc()
